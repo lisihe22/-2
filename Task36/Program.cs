8baff291ae4564db43ef3a80f956c6b1f630d9f4
Natalya <sorokanu95@mail.ru>
2023-02-04 16:02:35 +0500

    Здачка 36

---
+++ Task36/Program.cs
@@ -0,0 +1,25 @@
+﻿
+        void FillArray(int[] array)
+{
+             for (int i = 0; i < array.Length; i++)
+                 array[i] = new Random().Next(-100, 101); // [-100, 100]
+}
+
+        void ReleaseArray(int[] array)
+{
+        int sum = 0;
+             for (int i = 0; i < array.Length; i++)
+    {
+                if (i % 2 != 0)
+                     sum = sum + array[i];
+    }
+        Console.WriteLine($"Сумма элементов на нечетных позициях: {sum}");
+}
+
+Console.Clear();
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+FillArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+ReleaseArray(array);
