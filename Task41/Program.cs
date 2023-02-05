d2ce120fed1d751c7b46039d7a7c4c95cf24801e
Natalya <sorokanu95@mail.ru>
2023-02-05 17:23:01 +0500

    Задача 41

---
+++ Task41/Program.cs
@@ -0,0 +1,17 @@
+﻿   
+    void CalcNum(int m)
+{
+    int n = 0;
+    int count = 0;
+        for (int i = 1; i <= m; i++)
+    {
+            Console.Write($"Введите число {i}: ");
+            n = Convert.ToInt32(Console.ReadLine());
+        if (n == 0)
+            count++;
+    }
+            Console.Write($" \n Количество чисел равных 0: {count} ");
+}
+    Console.Write($"Введите количество чисел: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+CalcNum(n);
