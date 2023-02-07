2ff881d56b8caf74bf39882fa34b671cb0ba476f
Natalya <sorokanu95@mail.ru>
2023-02-07 12:44:22 +0500

    Задача 53

---
+++ Task53/Program.cs
@@ -0,0 +1,45 @@
+﻿
+
+void FillMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = new Random().Next(1, 11);
+    }
+  }
+}
+
+void ScreenMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
+  }
+}
+
+
+void ReleaseMatrix(int[,] matrix)
+{
+  int temp = 0;
+  for (int j = 0; j < matrix.GetLength(1); j++)
+  {
+    temp = matrix[0, j];
+    matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+    matrix[matrix.GetLength(0) - 1, j] = temp;
+  }
+}
+
+Console.Write("Ведите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+FillMatrix(matrix);
+ScreenMatrix(matrix);
+Console.WriteLine();
+ReleaseMatrix(matrix);
+ScreenMatrix(matrix);
