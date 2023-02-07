9c2e091f5a549248d503835152886920d0517bee
Natalya <sorokanu95@mail.ru>
2023-02-07 13:28:31 +0500

    Задача 55

---
+++ Task55/Program.cs
@@ -0,0 +1,54 @@
+﻿
+// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+void FillMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      matrix[i, j] = new Random().Next(1, 11);
+    }
+  }
+}
+void ScreenMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
+  }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+  int temp = 0;
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < i; j++)
+    {
+      temp = matrix[i, j];
+      matrix[i, j] = matrix[j, i];
+      matrix[j, i] = temp;
+  }
+}
+}
+
+Console.Clear();
+Console.Write("Ведите размеры массива: ");
+int[] size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+while (size[0] != size[1])
+{
+    Console.Write("Вы ошиблись!\nВедите размеры массива: ");
+    size = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+}
+int[,] matrix = new int[size[0], size[1]];
+FillMatrix(matrix);
+ScreenMatrix(matrix);
+Console.WriteLine();
+ReleaseMatrix(matrix);
+ScreenMatrix(matrix);
